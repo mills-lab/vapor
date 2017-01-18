c9adf482399e2bdca6679f091c1e7d4c0a36ddb0
xuefzhao <xuefzhao@umich.edu>
2017-01-18 13:22:05 -0500

    update-11182017

--- valor_vali/prep.pyx
+++ valor_vali/prep.pyx
@@ -1,18 +1,50 @@
+valor_version='valor V1.0 12-21-2015'
 def print_read_me():
-	print 'VaLoR V1.0 12-21-2015'
+	print valor_version
 	print 'Contact: Xuefang Zhao (xuefzhao@umich.edu)'
 	print ''
-	print 'Usage: VaLoR [Options] [Parameters]'
+	print 'Usage: valor [Options] [Parameters]'
 	print 'Options: '
 	print '	svelter'
 	print '	vcf'
 	print '	bed'
 	print 'Parameters:'
-	print '	--sv-input:'
-	print ' --output-path:'
-	print '	--pacbio-input:'
-	print '	--reference:'
-	print 'Optional Parameters:'
-	print '	--window-size'
-	print '	--sv-type'
-	print ' --size-cff'
+	print '	--sv-input:		input file in bed or vcf format'
+	print '	--output-path:		folder where the recurrence plot will be kept'
+	print '	--reference:		reference genome that pacbio files are aligned against'
+	print '	--pacbio-input:		absolute path of input pacbio file'
+
+def readme_melt():
+	print valor_version
+	print 'Contact: Xuefang Zhao (xuefzhao@umich.edu)'
+	print ''
+	print 'Usage: valor ins [Parameters]'
+	print 'Parameters:'
+	print ' --sv-input-prefix:	prefix of input file in vcf and fa format	'
+	print '	--output-path:		folder where the recurrence plot will be kept'
+	print '	--reference:		reference genome that pacbio files are aligned against'
+	print '	--pacbio-input:		absolute path of input pacbio file'
+
+def readme_bed():
+	print valor_version
+	print 'Contact: Xuefang Zhao (xuefzhao@umich.edu)'
+	print ''
+	print 'Usage: valor bed [Parameters]'
+	print 'Parameters:'
+	print '	--sv-input:		input file in bed format with SV type labeled in the last column'
+	print '	--output-path:		folder where the recurrence plot will be kept'
+	print '	--reference:		reference genome that pacbio files are aligned against'
+	print '	--pacbio-input:		absolute path of input pacbio file'
+
+
+def readme_vcf():
+	print valor_version
+	print 'Contact: Xuefang Zhao (xuefzhao@umich.edu)'
+	print ''
+	print 'Usage: valor vcf [Parameters]'
+	print 'Parameters:'
+	print '	--sv-input:		input file in vcf format'
+	print '	--output-path:		folder where the recurrence plot will be kept'
+	print '	--reference:		reference genome that pacbio files are aligned against'		
+	print '	--pacbio-input:		absolute path of input pacbio file'
+
